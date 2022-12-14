3d21221d7141069cd000c6eb128c6076e902d2ae
dawn <w7823125@gmail.com>
2022-12-14 14:48:11 +0800

    seperate esp uno

--- ArduinoCarV2Uno/ArduinoCarV2.ino
+++ ArduinoCarV2Uno/ArduinoCarV2.ino
@@ -1,28 +1,24 @@
-#include <analogWrite.h>
 #include <analogStick.h>
-#include "BluetoothSerial.h"
-
+#include <SoftwareSerial.h> //include SoftwareSerial Library
+SoftwareSerial BT(11, 12); //TX, RX
 byte BTData;
 String Data ;
 int values[2];
 
-int IN1 = 12;
-int IN2 = 14;
-int IN3 = 27;
-int IN4 = 26;
-int ENA = 25;
-int ENB = 33;
-int left_light = 2;
-int right_light = 4;
-int back_light = 5;
-int trig_pin = 32;
-int echo_pin = 35;
-int buzzer = 18;
+int IN1 = 3;
+int IN2 = 5;
+int IN3 = 6;
+int IN4 = 9;
+int ENA = A1;
+int ENB = A1;
+int left_light = 4;
+int right_light = 7;
+int back_light = 2;
+int trig_pin = 10;
+int echo_pin = 8;
+int buzzer = A0;
 
 BtAnalogStick car;
-
-BluetoothSerial SerialBT;
-
 void splitData(String str) {
   int index = str.indexOf(",");
 
@@ -31,16 +27,19 @@ void splitData(String str) {
 }
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
+  Serial.println("BT is ready!");
+  BT.begin(9600);
+  
   car.motorpin(IN1, IN2, IN3, IN4, ENA, ENB, left_light, right_light, back_light, buzzer);
   car.UltraSonicPin(trig_pin, echo_pin);
-  SerialBT.begin("AnalogBtCar");
   Serial.println("Bluetooth Started! Ready to pair...");
+  
 }
 
 void loop() {
-  if (SerialBT.available()) {
-    char character = SerialBT.read();
+  if (BT.available()> 0) {
+    char character = BT.read();
     Data.concat(character);
     if (character == '\n') {
       splitData(Data);
